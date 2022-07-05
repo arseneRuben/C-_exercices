1bf6e9c89666b7cc4d934faf6f24755228d8d372
Stéphane Duval <sduval@isi-montreal.com>
2022-07-05 12:48:07 -0400

    premier programme

---
+++ Visual Studio/Hello World/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Hello_World {
+    class Program {
+        static void Main(string[] args) {
+            dynamic n = 90;
+            n = true;
+            n = "toto";
+            n = 3.14159265;
+
+
+
+            Console.WriteLine("fini!");
+            Console.ReadLine();
+
+        }
+    }
+}
+
