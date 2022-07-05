dfeea03c311d59800ea440119e5421d732c19447
Stéphane Duval <sduval@isi-montreal.com>
2022-07-05 13:17:57 -0400

    exemple d'architecture pour exercices

---
+++ Visual Studio/01 - ex 1,2 et 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01___ex_1_2_et_3 {
+
+    namespace question1 {
+        class Question1 {
+            public static void Q1() {
+
+            }
+        }
+    }
+
+    namespace question2 {
+        class Question2 {
+            public static void Q2() {
+
+            }
+        }
+    }
+
+    namespace question3 {
+        class Question3 {
+            public static void Q3() {
+
+            }
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            question1.Question1.Q1();
+            question2.Question2.Q2();
+            question3.Question3.Q3();
+        }
+    }
+}
