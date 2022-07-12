e85d89290c7039143fc379d5eed3ab7b6de4d488
Stéphane Duval <sduval@isi-montreal.com>
2022-07-12 17:47:30 -0400

    illustration Kahoot chapitre 6

---
+++ Visual Studio/06 - code/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06___code {
+    abstract class Object {
+        public abstract void F();
+    }
+    class Program : Object {
+        public override void F() {
+
+        }
+
+        static void G() {
+
+        }
+        static void Main(string[] args) {
+
+            Object p = new Program();
+            p.F();
+            Program.G();
+
+
+        }
+    }
+}
