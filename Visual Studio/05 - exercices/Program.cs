d79ca027d08d4ad9fa542a38084e1a8f7fd49fd0
Stéphane Duval <sduval@isi-montreal.com>
2022-07-12 17:47:01 -0400

    illustration schema memoire pour chapitre 5

---
+++ Visual Studio/05 - exercices/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05___exercices {
+    class Dog {
+        internal String name;
+    }
+    class Student {
+        internal String name;
+        internal int age;
+        internal Dog myDog;
+    }
+    class Program {
+        static void Main(string[] args) {
+            Student s = new Student();
+            s.name = "toto";
+            s.age = 12;
+            s.myDog = new Dog();
+            s.myDog.name = "Milou";
+        }
+    }
+}
