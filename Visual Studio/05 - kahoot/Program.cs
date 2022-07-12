50f63d6a449494fc827b0c8e103419c0702f38c1
Stéphane Duval <sduval@isi-montreal.com>
2022-07-12 08:29:50 -0400

    illustration Kahoot chapitre 05 - Principes de la POO

---
+++ Visual Studio/05 - kahoot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05___kahoot {
+
+    using file1;
+
+    namespace file1 {
+        partial class Test {
+            internal int member1;
+        }
+
+    //}
+    //namespace file2 {
+        partial class Test {
+            internal int member2;
+        }
+
+    }
+
+
+    class Program {
+        static void Main(string[] args) {
+            Test t = new Test();
+            
+        }
+    }
+}
