0c31299500aa3cb2e19e726453904a6ce98dfbd2
Stéphane Duval <sduval@isi-montreal.com>
2022-07-12 17:47:53 -0400

    test C++ avec JF Lidou sur les pointeurs

--- Visual Studio/test/Source.cpp
+++ Visual Studio/test/Source.cpp
@@ -1,10 +1,29 @@
+#include <iostream>
+using namespace std;
+
 void f(int b, int a = 67, int c = 13) {
 
 }
 
+void g(int* p) {
+	cout << sizeof(p) << endl;
+
+}
+
 void main() {
+	int n = 568;
+	int* p = new int[10];
+	int q = 235;
+	//int p[10];
+	cout << sizeof(int) << endl;
+	cout << sizeof(long) << endl;
 
+	cout << p << endl;
+	cout << (&p) << endl;
 
+	cout << "&n = " << (long long)(&n) << endl;
+	cout << "&q = " << (long long)(&q) << endl;
 
-
+	cout << sizeof(p) << endl;
+	g(p);
 }
