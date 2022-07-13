c5544f9fa8cf775258e72a92e2cd82e40d70ac5b
Stéphane Duval <sduval@isi-montreal.com>
2022-07-13 12:33:59 -0400

    exemple de propriete avec un getter uniquement

---
+++ Visual Studio/05 - exemple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05___exemple {
+    class Program {
+        DateTime DoB {
+            get;
+        }
+
+        Program() {
+            this.DoB = DateTime.Now;
+        }
+
+        static void Main(string[] args) {
+            Program p = new Program();
+            //p.DoB = DateTime.Now;
+            Console.WriteLine( p.DoB);
+        }
+    }
+}
