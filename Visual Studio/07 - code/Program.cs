3522a5915944f3c06ae67bc4a82562bfb2b4bfa8
Stéphane Duval <sduval@isi-montreal.com>
2022-07-15 11:49:52 -0400

    illustration Kahoot chapitre 7 sur les delegues et evenements

---
+++ Visual Studio/07 - code/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07___code {
+    class Program {
+        static void Main(string[] args) {
+            String s = "toto";
+            String r = "ti";
+            r = r.Replace("i", "o");
+            String t = r + r;
+
+            Console.WriteLine(s == t);
+        }
+    }
+}
---
+++ Visual Studio/07 - exemple/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07___exemple {
+    public interface ITest {
+
+    }
+
+    public delegate void DGTest();
+
+    class Program {
+        static void Main(string[] args) {
+            EventHandler<Program> evt;
+        }
+    }
+}
