71ddb00ea09fd74df0e7ed1136809f8d777bbea6
Stéphane Duval <sduval@isi-montreal.com>
2022-07-19 11:32:59 -0400

    illustration Kahoot chapitre 10

---
+++ Visual Studio/10 - code/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10___code {
+    class Program {
+        void F() {
+            throw new NullReferenceException();
+        }
+
+        static void Main(string[] args){
+            Double d = 0;
+            Double x = Math.Sqrt(-64);
+            Double y = Double.NaN;
+            Console.WriteLine(x);
+
+            SqlConnection conn = new SqlConnection("");
+            conn.Open();
+
+            new Program().F();
+
+            throw new IndexOutOfRangeException();
+        }
+    }
+}
