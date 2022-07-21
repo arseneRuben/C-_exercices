08fcdd23a73aeb5d576562a75d2f924b9c381d2f
Stéphane Duval <sduval@isi-montreal.com>
2022-07-21 11:14:52 -0400

    exemple Heritage et Polymrphisme (chap 6)

---
+++ Visual Studio/06 - test hors assembly/Program.cs
@@ -0,0 +1,17 @@
+﻿using _06___Classe_abstraite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06___test_hors_assembly {
+    class Program : Teacher{
+        static void Main(string[] args) {
+            Teacher t = new Teacher();
+            //  t.address = ""; //  non!
+
+            new Program().address = "";
+        }
+    }
+}
