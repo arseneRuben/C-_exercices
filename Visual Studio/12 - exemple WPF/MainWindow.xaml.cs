426590e1e28996db02041e0e938788f1054a6b41
Stéphane Duval <sduval@isi-montreal.com>
2022-07-22 15:12:46 -0400

    exemple de projet 2 couches (WPF et Class Library)

---
+++ Visual Studio/12 - exemple WPF/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using _12___modele_WPF;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Input;
+
+namespace _12___exemple_WPF {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            this.InitializeComponent();
+            this.Left = 1920 + 200;
+            //this.Top = 200;
+
+            this.monLabel.Visibility = Visibility.Hidden;
+
+            this.monComboBox.Items.Add(new Personne { Nom = "Toto", Age = 28 });
+            this.monComboBox.Items.Add(new Personne { Nom = "Titi", Age = 21 });
+            this.monComboBox.Items.Add(new Personne { Nom = "Arsene", Age = 32 });
+
+            //this.monComboBox.SelectedItem = this.monComboBox.Items[0];
+            if (this.monComboBox.SelectedItem == null) {
+                Trace.WriteLine("Pas de selection dans le ComboBox");
+            }
+        }
+
+        private void ButtonHandler(object sender, RoutedEventArgs e) {
+            Trace.WriteLine("Hello World!");
+
+            this.monLabel.Content = "Hello World!";
+
+            this.monLabel.ToolTip = "Pas de tooltip !!!   :-(";
+
+            this.monLabel.Visibility = Visibility.Visible;
+        }
+
+        private void ButtonMouseEnter(object sender, MouseEventArgs e) {
+            this.monLabel.Content = "Entree dans la zone du bouton...";
+        }
+    }
+}
---
+++ Visual Studio/12 - modele/Personne.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _12___modele_WPF {
+    public class Personne {
+        public String Nom { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString() {
+            return String.Format("{0} ({1} ans)", this.Nom, this.Age);
+        }
+    }
+}
