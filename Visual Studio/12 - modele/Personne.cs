2f8d16585c4734b89985afc95c7c79f30240d0cd
Stéphane Duval <sduval@isi-montreal.com>
2022-07-25 09:31:05 -0400

    exemple sur les classes partielles

--- Visual Studio/12 - modele/Personne.cs
+++ Visual Studio/12 - modele/Personne.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 
 namespace _12___modele_WPF {
-    public class Personne {
+    public partial class Personne {
         public String Nom { get; set; }
         public int Age { get; set; }
+
+        private int id;
 
         public override string ToString() {
             return String.Format("{0} ({1} ans)", this.Nom, this.Age);
---
+++ Visual Studio/12 - modele/PersonneSuite.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace _12___modele_WPF {
+    partial class Personne {
+        public int AutreDonnee { get; set; }
+
+        private void F() {
+            this.id = 2345;
+        }
+    }
+}
